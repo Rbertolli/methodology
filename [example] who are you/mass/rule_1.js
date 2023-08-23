af4bbb080faf77fb5b6bba6f43d52b6c500446c7
Rafael Bertolli <rafael@bertolli.com.br>
2023-08-23 14:15:38 -0300

    Update rule_1.js

--- [example] who are you/mass/rule_1.js
+++ [example] who are you/mass/rule_1.js
@@ -1,4 +1,11 @@
-console.log("Received event: ", event);
+/*
+Regra de verificação do tipo de resíduo contido em uma massa
+O script deverá verificar, no JSON de um Documento se
+Category: Mass
+Type: !Reject
+*/
+
+   console.log("Received event: ", event);
    var data = {
        "greetings": "Hello, " + event.firstName + " " + event.lastName + "."
    };
