1b1e6e478b8677957e2eacda81c72e5d08e623fc
Rafael Bertolli <rafael@bertolli.com.br>
2023-08-23 14:16:41 -0300

    Update rule_1.js

--- [example] who are you/certificate/rule_1.js
+++ [example] who are you/certificate/rule_1.js
@@ -1,1 +1,9 @@
+/*
+Regra de verificação se todos os mass id's são do mesmo Reciclador.
+*/
 
+   console.log("Received event: ", event);
+   var data = {
+       "greetings": "Hello, " + event.firstName + " " + event.lastName + "."
+   };
+   callback(null, data);
